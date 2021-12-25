b4de347913ea86b0c6f52dfe607b5c6d53c16602
Atamurad <atamurad.babakulyyev@gmail.com>
2021-12-24 19:54:13 -0500

    footer implement wave

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 function Footer() {
   return (
     <footer>
-      FOOOTERR
+      <div className="waves">
+        <div className="wave" id="wave1"></div>
+        <div className="wave" id="wave2"></div>
+        <div className="wave" id="wave3"></div>
+        <div className="wave" id="wave4"></div>
+      </div>
     </footer>
   )
 }
